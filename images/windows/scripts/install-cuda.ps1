fbd2459fe7830e4e2dedb9fcdcce828617258313
Wesley Maxey <wesley.maxey@gmail.com>
2023-05-18 19:47:57 -0700

    Prevent partial installation in cuda installer

--- images/windows/scripts/install-cuda.ps1
+++ images/windows/scripts/install-cuda.ps1
@@ -1,5 +1,5 @@
 Invoke-WebRequest -Uri "https://developer.download.nvidia.com/compute/cuda/12.1.0/network_installers/cuda_12.1.0_windows_network.exe" -OutFile "./cuda_network.exe" -UseBasicParsing
-Start-Process -Wait -FilePath .\cuda_network.exe -ArgumentList "-s nvcc_12.1 cudart_12.1"
+Start-Process -Wait -PassThru -FilePath .\cuda_network.exe -ArgumentList "-s nvcc_12.1 cudart_12.1"
 
 $ENV:PATH="$ENV:PATH;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.1\bin"
 $ENV:CUDA_PATH="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.1"
