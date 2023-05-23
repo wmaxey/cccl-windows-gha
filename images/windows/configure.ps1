f349a05f9293d6d652c6fdaa35b25156bf8ff8ef
Wesley Maxey <wesley.maxey@gmail.com>
2023-05-22 18:53:02 -0700

    Filter environment variables that are not portable.

--- images/windows/configure.ps1
+++ images/windows/configure.ps1
@@ -21,8 +21,14 @@ Push-location "$PSScriptRoot"
 ## Save the current environment without MSVC plugged in
 New-Item -ItemType Directory -Path "$HOME" -Name "cccl_env"
 
+# Filter these non-portable exported environment variables
+$envFilter = `
+    "COMPUTERNAME","TEMP","TMP","SystemDrive","SystemRoot","USERNAME","USERPROFILE",`
+    "APPDATA","LOCALAPPDATA","NUMBER_OF_PROCESSORS","PROCESSOR_ARCHITECTURE",`
+    "PROCESSOR_IDENTIFIER","PROCESSOR_LEVEL","PROCESSOR_REVISION","OS"
+
 $ENV:INSTALLED_MSVC_VERSION=$msvcVersion
-Get-ChildItem env: | Export-CliXml "$HOME\cccl_env\env-var.clixml"
+Get-ChildItem ENV: | Where-Object { $_.Name -notin $envFilter } | Export-CliXml "$HOME\cccl_env\env-var.clixml"
 
 ## Install MSVC
 ./scripts/install-vs.ps1 -msvcVersion $msvcVersion
