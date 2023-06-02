14bd91c6c3928d96b7db6bb1216abe5ff71bb37f
Wesley Maxey <wesley.maxey@gmail.com>
2023-06-02 15:21:22 -0700

    Connect Windows edition into the build script.

--- images/docker_build.ps1
+++ images/docker_build.ps1
@@ -2,7 +2,11 @@ Param(
     [Parameter(Mandatory=$true)]
     [ValidateSet('2017', '2019', '2022')]
     [string[]]
-    $msvcVersion
+    $msvcVersion,
+    [Parameter(Mandatory=$false)]
+    [ValidateSet('windows', 'windows-server')]
+    [string[]]
+    $edition="windows"
 )
 
 Push-location "$PSScriptRoot"
@@ -10,13 +14,14 @@ Push-location "$PSScriptRoot"
 # Source version matrix
 ./vs_version_matrix.ps1
 
-docker compose -f .\docker-compose.yml build windows-$msvcVersion --progress=plain
+docker compose -f .\docker-compose.yml build $edition-$msvcVersion --progress=plain
 
 # Build each subcompiler version
 $clList = $vsVersionMatrix["$msvcVersion"]
 foreach($cl in $clList) {
     Write-Output "Building $msvcVersion - $cl"
     # Override defaults in .env.
+    $ENV:WIN_IMAGE=$edition
     $ENV:MSVC_COMPILER_VER=$cl
     docker compose -f .\docker-compose.cl.yml build windows-extend-$msvcVersion --progress=plain
 }
